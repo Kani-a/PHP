b6d78907e6bddf9220030c8a2462e09e9b3c6f88
kani-a <allanchagasilva@gmail.com>
2024-09-04 15:48:00 -0300

    sou bixa

--- src/index.php
+++ src/index.php
@@ -1,2 +1,56 @@
 <?php
-echo "socoro";
+
+namespace App\Backend\src; 
+
+require_once '../';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+switch($method){
+	case 'GET':
+		switch($uri){
+
+		case '/user':
+		http_response_code[200];
+		echo json_encode(['status' => true, 'mensagen' => 'recebido com suceso']);
+		break;
+
+		case '/produto':
+			http_response_code[200];
+			echo json_encode(['status' => true, 'mensagen' => 'recebido com suceso']);
+		}
+
+	break;
+
+	case 'POST':
+        switch($uri){
+            case '/users';
+                $data = json_decode(file_get_contents('php://input'),true);
+                http_response_code[200];
+                echo json_encode(['stauts' => true,'message' => 'receita com sucesso','dado' => $data]);
+                break;
+
+                case '/produtos';
+                $data = json_decode(file_get_contents('php://input'),true);
+                http_response_code[200];
+                echo json_encode(['stauts' => true,'message' => 'receita com sucesso','dado' => $data]);
+                break;
+
+                default:
+                echo json_encode(["uri invalido"]);
+            }
+	break; 
+	case 'PUT':
+
+	break;
+
+	case 'DELETE':
+
+	break;
+
+
+	default:
+	echo json_encode(["methodo invalidao"]);
+} 
